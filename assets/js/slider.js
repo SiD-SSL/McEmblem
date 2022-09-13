6ffc3188c0d4d7a46154f39faf04c72f38b6f911
unknown <siddik.ssltd@gmail.com>
2022-09-13 18:36:53 +0600

    update

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -51,3 +51,58 @@ $('.banner-slider').slick({
 	  ]
 });
 
+
+
+// ====================================================
+//       		NEW ARRIVAL SLIDER JS                //
+// ====================================================
+$('.arrival-slider').slick({
+    dots: false,
+	arrows: true,
+    autoplay: false,
+	autoplaySpeed: 1500,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    vertical: false,
+    prevArrow: '<i class="fas fa-caret-left left-arrow"></i>',
+	nextArrow: '<i class="fas fa-caret-right right-arrow"></i>',
+	responsive: [
+		{
+		  breakpoint: 1200,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false,
+		  }
+		},
+		{
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false,
+		  }
+		},
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+    		centerMode: false,
+		  }
+		},
+		{
+		  breakpoint: 576,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+    		centerMode: false,
+		  }
+		}
+	  ]
+});
+
