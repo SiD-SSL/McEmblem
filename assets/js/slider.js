4a0585a01bf610e3d2a8916850455bfc239f2605
unknown <siddik.ssltd@gmail.com>
2022-09-14 14:35:07 +0600

    update

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -106,3 +106,59 @@ $('.arrival-slider').slick({
 	  ]
 });
 
+
+
+
+// ====================================================
+//       			  TEAM SLIDER JS                 //
+// ====================================================
+$('.team-slider').slick({
+    dots: false,
+	arrows: true,
+    autoplay: false,
+	autoplaySpeed: 1500,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: false,
+    prevArrow: '<i class="fas fa-caret-left left-arrow"></i>',
+	nextArrow: '<i class="fas fa-caret-right right-arrow"></i>',
+	responsive: [
+		{
+		  breakpoint: 1200,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false,
+		  }
+		},
+		{
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false,
+		  }
+		},
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+    		centerMode: false,
+		  }
+		},
+		{
+		  breakpoint: 576,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+    		centerMode: false,
+		  }
+		}
+	  ]
+});
+
